cf19559925bd48fadca766a5f2d60a53d2f2f07d
Wdfly <2737278699@qq.com>
2026-01-16 13:14:08 +0800

    项目结构

---
+++ src/main/java/nuc/edu/lumecho/LumEchoApplication.java
@@ -0,0 +1,13 @@
+package nuc.edu.lumecho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumEchoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LumEchoApplication.class, args);
+    }
+}
+
