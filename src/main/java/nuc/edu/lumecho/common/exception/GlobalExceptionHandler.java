eabfcbe2aa059c1aa26803a5086cd1ea8084c29f
Wdfly <wangdf57487@hundsun.com>
2026-03-02 17:57:37 +0800

    全局异常处理

--- src/main/java/nuc/edu/lumecho/common/exception/GlobalExceptionHandler.java
+++ src/main/java/nuc/edu/lumecho/common/exception/GlobalExceptionHandler.java
@@ -1,4 +1,15 @@
 package nuc.edu.lumecho.common.exception;
 
+import nuc.edu.lumecho.common.Enum.ResultCodeEnum;
+import nuc.edu.lumecho.common.Result;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public Result handleBusinessException(BusinessException ex) {
+        return Result.fail(ex.getCode(), ex.getMessage());
+    }
 }
