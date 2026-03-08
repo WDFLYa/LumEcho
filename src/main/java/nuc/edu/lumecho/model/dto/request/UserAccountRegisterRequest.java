a8cf211dabfa4d87089a3c469f0830ef88766bd1
WDFLY <2737278699@qq.com>
2026-03-08 22:57:24 +0800

    正则更改

--- src/main/java/nuc/edu/lumecho/model/dto/request/UserAccountRegisterRequest.java
+++ src/main/java/nuc/edu/lumecho/model/dto/request/UserAccountRegisterRequest.java
@@ -9,10 +9,10 @@ import javax.validation.constraints.Size;
 @Data
 public class UserAccountRegisterRequest {
     @NotBlank(message = "账号不能为空")
-    @Pattern(regexp = "^[a-zA-Z0-9_]{3,20}$", message = "账号只能包含字母、数字、下划线，长度3-20位")
+    @Pattern(regexp = "^(|[a-zA-Z0-9_]{3,20})$", message = "账号只能包含字母、数字、下划线，长度3-20位")
     private String account;
 
     @NotBlank(message = "密码不能为空")
-    @Size(min = 6, max = 20, message = "密码长度必须在6-20位之间")
+    @Pattern(regexp = "^(|.{6,20})$", message = "密码长度必须在6-20位之间")
     private String password;
 }
