aa64abd9f0fd3e809e95a1ddf7aefd80737609fe
Wdfly <wangdf57487@hundsun.com>
2026-04-15 15:56:14 +0800

    摄影活动相关接口

--- src/main/java/nuc/edu/lumecho/controller/photographyactivity/PhotographyActivityController.java
+++ src/main/java/nuc/edu/lumecho/controller/photographyactivity/PhotographyActivityController.java
@@ -2,12 +2,13 @@ package nuc.edu.lumecho.controller.photographyactivity;
 
 import nuc.edu.lumecho.common.Result;
 import nuc.edu.lumecho.model.dto.request.photographer.CreateActivityRequest;
+import nuc.edu.lumecho.model.dto.response.activity.PhotographyActivityListResponse;
+import nuc.edu.lumecho.model.entity.PhotographyActivity;
 import nuc.edu.lumecho.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/activity")
@@ -20,5 +21,17 @@ public class PhotographyActivityController {
     public Result createActivity(@RequestBody CreateActivityRequest createActivityRequest) {
         activityService.createActivity(createActivityRequest);
         return Result.ok();
+    }
+
+    @GetMapping("/list")
+    public Result<List<PhotographyActivityListResponse>> getActivityList() {
+        List<PhotographyActivityListResponse> list = activityService.getActivityList();
+        return Result.ok(list);
+    }
+
+    @GetMapping("/detail/{id}")
+    public Result<PhotographyActivityListResponse> getActivityDetail(@PathVariable Long id) {
+        PhotographyActivityListResponse detail = activityService.getActivityDetail(id);
+        return Result.ok(detail);
     }
 }
