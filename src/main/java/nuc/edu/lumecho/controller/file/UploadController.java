be5f0860aec394e0e4a42230613e676320cd1a57
WDFLY <2737278699@qq.com>
2026-04-17 12:40:41 +0800

    头像更新

--- src/main/java/nuc/edu/lumecho/controller/file/UploadController.java
+++ src/main/java/nuc/edu/lumecho/controller/file/UploadController.java
@@ -1,6 +1,7 @@
 package nuc.edu.lumecho.controller.file;
 
 import io.minio.errors.*;
+import nuc.edu.lumecho.common.Enum.ResourceTypeEnum;
 import nuc.edu.lumecho.common.Result;
 import nuc.edu.lumecho.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,13 +21,32 @@ public class UploadController {
     @Autowired
     private FileService fileService;
 
+    /**
+     * 通用文件上传接口
+     */
     @PostMapping("/upload")
     public Result<String> uploadFile(
             @RequestParam("file") MultipartFile file,
             @RequestParam("bizType") String bizType
     ) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
-            String url = fileService.uploadFile(file, bizType);
-            return Result.ok(url);
 
+        String url = fileService.uploadFile(file, bizType);
+        return Result.ok(url);
+    }
+
+    /**
+     * 用户头像上传/更新接口
+     * 此接口不仅上传文件，还会关联当前登录用户 ID (bizId)
+     */
+    @PostMapping("/avatar/upload")
+    public Result<String> uploadAvatar(
+            @RequestParam("file") MultipartFile file
+    ) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+
+
+        // 调用 Service 层的更新头像方法
+        // 该方法内部会自动获取当前用户 ID 并更新数据库记录
+        String url = fileService.updateUserAvatar(file);
+        return Result.ok(url);
     }
 }
--- src/main/resources/sql/user.sql
+++ src/main/resources/sql/user.sql
@@ -6,26 +6,6 @@ CREATE TABLE `user` (
                         `username` VARCHAR(50) DEFAULT NULL COMMENT '用户名',
                         `email` VARCHAR(100) DEFAULT NULL COMMENT '邮箱',
                         `bio` VARCHAR(255) DEFAULT NULL COMMENT '简介',
-                        `status` TINYINT DEFAULT NULL DEFAULT 1 COMMENT '状态',
-                        `role` VARCHAR(20) DEFAULT NULL DEFAULT 'USER' COMMENT '角色',
-                        `create_time` DATETIME DEFAULT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-                        `update_time` DATETIME DEFAULT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-                        `deleted_at` DATETIME DEFAULT NULL COMMENT '逻辑删除时间',
-                        PRIMARY KEY (`id`)
-);
-
-ALTER TABLE `user`
-    ADD COLUMN `fans_count` BIGINT(20) DEFAULT 0 COMMENT '粉丝数',
-ADD COLUMN `follow_count` BIGINT(20) DEFAULT 0 COMMENT '关注数';
-
-CREATE TABLE `user` (
-                        `id` BIGINT NOT NULL AUTO_INCREMENT COMMENT '用户ID',
-                        `account` VARCHAR(50) DEFAULT NULL COMMENT '登录账号',
-                        `phone` VARCHAR(20) DEFAULT NULL COMMENT '手机号',
-                        `password` VARCHAR(100) DEFAULT NULL COMMENT '密码',
-                        `username` VARCHAR(50) DEFAULT NULL COMMENT '用户名',
-                        `email` VARCHAR(100) DEFAULT NULL COMMENT '邮箱',
-                        `bio` VARCHAR(255) DEFAULT NULL COMMENT '简介',
                         `status` TINYINT DEFAULT 1 COMMENT '状态',
                         `role` VARCHAR(20) DEFAULT 'USER' COMMENT '角色',
                         `create_time` DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
